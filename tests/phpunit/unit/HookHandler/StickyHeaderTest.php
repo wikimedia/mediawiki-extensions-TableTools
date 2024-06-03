0680d2f5843747290d81fee3e9bdb92c9cb0d87d
Daniel Riedmüller <daniel.riedmueller@hallowelt.com>
2024-06-03 11:05:19 +0200

    Initial commit
    Change-Id: I18fe35575ae7a815de6f7e138db0ef41666ab101

---
+++ tests/phpunit/unit/HookHandler/StickyHeaderTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace MediaWiki\Extension\TableTools\Tests\HookHandler;
+
+use MediaWiki\Extension\TableTools\HookHandler\StickyHeader;
+use OutputPage;
+
+/**
+ * @coversDefaultClass \MediaWiki\Extension\TableTools\HookHandler\StickyHeader
+ */
+class StickyHeaderTest extends \MediaWikiUnitTestCase {
+
+	/**
+	 * @covers MediaWiki\Extension\TableTools\HookHandler\StickyHeader::onOutputPageBeforeHTML
+	 */
+	public function testOnBeforePageDisplayPositive() {
+		$outputPageMock = $this->getMockBuilder( OutputPage::class )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$outputPageMock->expects( $this->once() )
+			->method( 'addModules' )
+			->with( 'ext.tableTools.stickyHeader' );
+
+		$text = <<<HERE
+Some text, some [[Link]] and a table with a sticky header:
+<table class="wikitext mw-sticky-header">
+	<tr><th>Header</th></tr>
+	<tr><td>Content</td></tr>
+</table>
+Additional text.
+HERE;
+
+		( new StickyHeader() )->onOutputPageBeforeHTML( $outputPageMock, $text );
+	}
+
+	/**
+	 * @covers MediaWiki\Extension\TableTools\HookHandler\StickyHeader::onOutputPageBeforeHTML
+	 */
+	public function testOnBeforePageDisplayNegative() {
+		$outputPageMock = $this->getMockBuilder( OutputPage::class )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$outputPageMock->expects( $this->never() )
+			->method( 'addModules' )
+			->with( 'ext.tableTools.stickyHeader' );
+
+		$text = <<<HERE
+Some text, some [[Link]] and a table with a sticky header:
+<table class="wikitext">
+	<tr><th>Header</th></tr>
+	<tr><td>Content</td></tr>
+</table>
+Additional text.
+HERE;
+
+		( new StickyHeader() )->onOutputPageBeforeHTML( $outputPageMock, $text );
+	}
+}
