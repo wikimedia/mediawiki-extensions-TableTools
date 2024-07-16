a80820dfba4f417621e93a14ed65454a9f225bef
Daniel Vogel <daniel.vogel@hallowelt.com>
2024-07-16 08:39:00 +0200

    Improve datatable
    Depends-on: Ie2b1447478a9bde32e17c7a8572e8af499078989
    Change-Id: I6931c1bee4d90a2951e81c74663653ff644da610

--- resources/ext.tableTools.dataTable.js
+++ resources/ext.tableTools.dataTable.js
@@ -1,83 +1,111 @@
 /* eslint-disable */
 
 ( function ( mw, $, d, undefined ) {
-	const Vue = require( 'vue' );
-	const GridApp = require( 'ext.vuejsplus-data-grid.vue' );
+    const Vue = require( 'vue' );
+    const GridApp = require( 'ext.vuejsplus-data-grid.vue' );
 
-	const tables = $( '.datagrid, .data-grid' );
-	for ( tableIndex = 0; tableIndex < tables.length; tableIndex++ ) {
-		let invalidFormat = false;
-		const fragment = document.createDocumentFragment();
+    var tables = $( '.datagrid, .data-grid' );
+    for ( tableIndex = 0; tableIndex < tables.length; tableIndex++ ) {
+        var invalidFormat = false;
+        const fragment = document.createDocumentFragment();
 
-		var table = tables[ tableIndex ];
-		let tableClassName = $( table ).attr( 'class' );
+        var table = tables[tableIndex];
+        var tableClassName = $( table ).attr( 'class' );
 
-		if ( tableClassName === undefined ) {
-			tableClassName = '';
-		}
+        if ( tableClassName === undefined ) {
+            tableClassName = '';
+        }
 
-		var spec = {
-			class: tableClassName,
-			cols: [],
-			rows: []
-		};
+        var spec = {
+            class: tableClassName,
+            cols: [],
+            rows: []
+        };
 
-		const rows = $( table ).find( 'tr' );
-		for ( rowIndex = 0; rowIndex < rows.length; rowIndex++ ) {
-			const row = rows[ rowIndex ];
-			let cells = $( row ).find( 'th' );
-			if ( cells.length === 0 ) {
-				cells = $( row ).find( 'td' );
-			}
-			const rowCells = [];
-			for ( cellIndex = 0; cellIndex < cells.length; cellIndex++ ) {
-				var cell = cells[ cellIndex ];
-				if ( $( cell ).attr( 'rowspan' ) || $( cell ).attr( 'colspan' ) ) {
-					invalidFormat = true;
-				}
-				if ( rowIndex === 0 ) {
-					const col = {
-						label: $( cell ).html(),
-						dataIndex: cellIndex + 1,
-						type: 'string',
-						sortable: true,
-						filterable: true
-					};
-					spec.cols.push( col );
-				} else {
-					var cell = {
-						dataIndex: cellIndex + 1,
-						content: $( cell ).html()
-					};
-					rowCells.push( cell );
-				}
-			}
-			if ( rowCells.length > 0 ) {
-				spec.rows.push( {
-					class: '',
-					data: rowCells
-				} );
-			}
-		}
+        var colTypes = [];
+        var rows = $( table ).find( 'tr' );
+        for ( rowIndex = 0; rowIndex < rows.length; rowIndex++ ) {
+            var row = rows[rowIndex];
+            var cells = $( row ).find( 'th' );
+            if ( cells.length === 0 ) {
+                cells = $( row ).find( 'td' );
+            }
+            var rowCells = [];
+            for ( cellIndex = 0; cellIndex < cells.length; cellIndex++ ) {
+                var cell = cells[cellIndex];
+                if ( $( cell ).attr( 'rowspan' ) || $( cell ).attr( 'colspan' ) ) {
+                    invalidFormat = true;
+                }
+                let cellClass = $( cell ).attr( 'class' );
+                if ( !cellClass ) {
+                    cellClass = '';
+                }
 
-		function render() {
-			const h = Vue.h;
+                if ( rowIndex === 0 ) {
+                    let colType = 'html';
+                    if ( $( cell ).hasClass( 'int' ) || $( cell ).hasClass( 'numeric' ) ) {
+                        colType = 'numeric'
+                    } else if ( $( cell ).hasClass( 'string' ) ) {
+                        colType = 'string'
+                    } else if ( $( cell ).hasClass( 'boolean' ) ) {
+                        colType = 'boolean'
+                    }
+                    colTypes.push( colType );
 
-			const vm = Vue.createMwApp( {
-				render: function () {
-					return h( GridApp, spec );
-				}
-			} );
+                    var col = {
+                        label: $( cell ).html(),
+                        dataIndex: cellIndex+1,
+                        type: colType,
+                        sortable: true,
+                        filterable: true,
+                        class: cellClass
+                    }
+                    spec.cols.push( col );
+                } else {
+                    let cellContent = $( cell ).html();
+                    if ( colTypes[cellIndex] === 'boolean' ) {
+                        if ( cellContent.trim() === 'true' || cellContent.trim() === '1' ) {
+                            cellContent = true;
+                        } else if ( cellContent.trim() === 'false' || cellContent.trim() === '0' ) {
+                            cellContent = false;
+                        } else {
+                            cellContent = undefined;
+                        }
+                    }
+                    var cell = {
+                        dataIndex: cellIndex+1,
+                        content: cellContent,
+                        class: cellClass
+                    }
+                    rowCells.push( cell );
+                }
+            }
+            if ( rowCells.length > 0 ) {
+                spec.rows.push( {
+                    class: '',
+                    data: rowCells
+                } );
+            }
+        }
 
-			vm.mount( fragment );
+        function render() {
+            var h = Vue.h;
 
-			// https://www.reddit.com/r/vuejs/comments/i1vwyw/vue_3_createapp_does_not_replace_mount_element/?rdt=38765
-			table.parentNode.replaceChild( fragment, table );
-		}
+            var vm = Vue.createMwApp( {
+                render: function() {
+                    return h( GridApp, spec );
+                }
+            } );
 
-		// rowspan and colspan is not supported
-		if ( invalidFormat === false ) {
-			render();
-		}
-	}
-}( mediaWiki, jQuery, document ) );
+            vm.mount( fragment );
+
+            // https://www.reddit.com/r/vuejs/comments/i1vwyw/vue_3_createapp_does_not_replace_mount_element/?rdt=38765
+            table.parentNode.replaceChild( fragment, table );
+        };
+
+        // rowspan and colspan is not supported
+        if ( invalidFormat === false ) {
+            render();
+        }
+    }
+} )( mediaWiki, jQuery, document );
